60ff5aa1d9672eaa306bd0cb0ef50a0d56a0767d
Jheath122576 <65102308+Jheath122576@users.noreply.github.com>
2020-08-04 17:37:35 -0400

    Game complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const colors = ["blue", "red", "orange", "green", "yellow"];
 const cards = document.querySelectorAll(".card");
 const resetButton =document.querySelector(".reset");
+const scoreSpan = document.querySelector(".score");
 
 cards.forEach((card) => {
     card.addEventListener("click", () => {
@@ -19,3 +20,18 @@ function getCardColor(card) {
     let color = colors[x];
     card.style.backgroundColor = color;
 }
+
+setInterval(() => {
+    let color1 = cards[0].style.backgroundColor;
+    let color2 = cards[1].style.backgroundColor;
+    let color3 = cards[2].style.backgroundColor;
+    if (color1 === "" || color2 === "" || color3 === "") return false;
+    if (color1 !== "" && color1 === color2 && color1 === color3) {
+        scoreSpan.innerText = "500";
+    } else if (color1 !=="" && (color1 === color2 || color1 === color3)) {
+        scoreSpan.innerText = "200";
+    } else if (color2 !== "" && color2 === color3) {
+        scoreSpan.innerText = "200";
+    }
+}, 100);
+
